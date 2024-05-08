7440f2f01ccd1e22c7c42329d16c0cfaee5a9882
João Costa <130461653+joaocostarodrigues@users.noreply.github.com>
2024-05-08 20:39:27 -0300

    1.0

---
+++ Galeria_React_Responsiva/react-galeria/galeria/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Galeria from './components/Galeria'
+
+function App() {
+  return (
+    <>
+      <Galeria/>
+    </>
+  )
+}
+
+export default App
---
+++ Galeria_React_Responsiva/react-galeria/galeria/src/components/Galeria.jsx
@@ -0,0 +1,24 @@
+export default function Galeria(){
+    return(
+<div className="gallery">
+      <div className="image">
+        <img src="imagem1.jpg" alt="Imagem 1" />
+      </div>
+      <div className="image">
+        <img src="imagem2.jpg" alt="Imagem 2" />
+      </div>
+      <div className="image">
+        <img src="imagem3.jpg" alt="Imagem 3" />
+      </div>
+      <div className="image">
+        <img src="imagem4.jpg" alt="Imagem 4" />
+      </div>
+      <div className="image">
+        <img src="imagem5.jpg" alt="Imagem 5" />
+      </div>
+      <div className="image">
+        <img src="imagem6.jpg" alt="Imagem 6" />
+      </div>
+    </div>
+    )
+}
